868a86ec81dfee75a4748c932136437ac740a3ba
Murilo Biss <murilobiss@gmail.com>
2023-05-31 11:53:13 -0300

    first commit

---
+++ twitter_etl.py
@@ -0,0 +1,57 @@
+import io
+import pyarrow
+import tweepy
+import pandas as pd
+import json
+from datetime import datetime
+import boto3
+
+
+def run_twitter_etl():
+    access_key = 'w1560SVcH0OBa9SIS93B4AbqS'
+    access_secret = 'c9DY4Z7EYTMmncBBZp4bIZzBbDhO14Q7jNh0QFhNXDWEQ1fvC0'
+    consumer_key = '920418075471745025-JiXeI5Me6vm8EcgBWlSoH81r8S1SDiV'
+    consumer_secret = 'DOHIiYFYEqzPkac0PADmIF84EftsFuQYeabBXLrUXGbun'
+
+    #Twitter Authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)
+    auth.set_access_token(consumer_key, consumer_secret)
+
+    #Creating an API Object
+    api = tweepy.API(auth)
+
+    tweets = api.user_timeline(screen_name='@RedeCoxa',
+                            count=200,
+                            include_rts=False,
+                            tweet_mode='extended'
+                            )
+
+    tweet_list = []
+
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {'user': tweet.user.screen_name,
+                        'text': text,
+                        'favorite_count': tweet.favorite_count,
+                        'retweet_count': tweet.retweet_count,
+                        'created_at': tweet.created_at                     
+                        }
+        
+        tweet_list.append(refined_tweet)
+
+    df = pd.DataFrame(tweet_list)
+
+    # Save DataFrame to parquet file
+    s3 = boto3.client('s3')
+    bucket_name = 'bancobari-raw-381733490365'
+    file_name = 'twitter/redecoxa.parquet'
+
+    # Save DataFrame to parquet file in memory
+    parquet_buffer = io.BytesIO()
+    df.to_parquet(parquet_buffer, index=False)
+
+    # Upload the file to S3
+    s3.upload_fileobj(parquet_buffer, bucket_name, file_name)
+
+run_twitter_etl()
---
+++ twitter_redshift.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import psycopg2
+from airflow.hooks.base_hook import BaseHook
+
+# Redshift credentials
+host = 'your-hostname'
+port = 'your-port'
+database = 'your-database'
+user = 'your-username'
+password = 'your-password'
+
+s3_bucket = 'bancobari-raw-381733490365'
+s3_file = 'twitter/redecoxa.parquet'
+
+# Loading parquet file from S3 to a DF
+s3_path = f's3://{s3_bucket}/{s3_file}'
+df = pd.read_parquet(s3_path)
+
+# Redshift Connection
+conn = psycopg2.connect(
+    host=host,
+    port=port,
+    database=database,
+    user=user,
+    password=password
+)
+
+# Creating tables
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS redecoxa (
+        user VARCHAR(255),
+        text VARCHAR(255),
+        favorite_count INT,
+        retweet_count INT,
+        created_at TIMESTAMP
+    )
+""")
+
+conn.commit()
+
+# Carregamento do DataFrame para o Redshift
+cur = conn.cursor()
+
+# Drop temporário para permitir carregamento de Parquet
+cur.execute("DROP TABLE IF EXISTS redecoxa_temp")
+
+# Carregamento do Parquet para uma tabela temporária
+table_name = 'redecoxa_temp'
+pq.write_table(pa.Table.from_pandas(df), f's3://{s3_bucket}/{table_name}.parquet')
+copy_query = f"""
+    COPY {table_name}
+    FROM 's3://{s3_bucket}/{table_name}.parquet'
+    IAM_ROLE 'arn:aws:iam::381733490365:role/spectrumRoles'
+    FORMAT AS PARQUET
+"""
+cur.execute(copy_query)
+
+# Inserção dos dados da tabela temporária para a tabela final
+insert_query = f"""
+    INSERT INTO redecoxa
+    SELECT * FROM {table_name}
+"""
+cur.execute(insert_query)
+
+conn.commit()
+
